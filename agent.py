97357374ddf6e7c98a014829536ece63824850aa
Fei Wang <wang.flynn@gmail.com>
2021-10-08 01:46:38 +0800

    tune variable

--- agent.py
+++ agent.py
@@ -1878,8 +1878,8 @@ class Strategy:
             citytile.pos]
 
       v = (wt / dd(arrival_turns) +
-           (city_crash_boost / dd(arrival_turns, r=1.03)) +
-           (city_survive_boost / dd(arrival_turns, r=1.03)) +
+           (city_crash_boost / dd(arrival_turns, r=1.1)) +
+           (city_survive_boost / dd(arrival_turns, r=1.1)) +
            (receive_transfer_wt / dd(arrival_turns)))
       if (city_cell.is_first_citytile and worker.id in DRAW_UNIT_LIST and
           city_cell.pos in MAP_POS_LIST):
